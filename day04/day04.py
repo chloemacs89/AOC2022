bbc993fb91f06bed46cd5906a195db382a1ef398
chloemacs89 <jer.villey@gmail.com>
2022-12-04 15:42:10 +0100

    day 4, two stars !

--- day04/day04.py
+++ day04/day04.py
@@ -1,4 +1,7 @@
 with open("./data.txt", "r", encoding="utf-8") as data_file:
-    solve1 = sum([1 for y in [x.strip("\n").replace("-", " ").replace(",", " ").split(" ") for x in data_file.readlines()] if (set(range(int(y[0]), int(y[1])+1)).issuperset(set(range(int(y[2]), int(y[3])+1))) or set(range(int(y[0]), int(y[1])+1)).issubset(set(range(int(y[2]), int(y[3])+1))))]) # NOQA
+    pairs = [x.strip("\n").replace("-", " ").replace(",", " ").split(" ") for x in data_file.readlines()] # NOQA
+    solve1 = sum([1 for y in pairs if (set(range(int(y[0]), int(y[1])+1)).issuperset(set(range(int(y[2]), int(y[3])+1))) or set(range(int(y[0]), int(y[1])+1)).issubset(set(range(int(y[2]), int(y[3])+1))))]) # NOQA
+    solve2 = sum([1 for y in pairs if (set(range(int(y[0]), int(y[1])+1)) & (set(range(int(y[2]), int(y[3])+1))))]) # NOQA
 
 print(solve1)
+print(solve2)
