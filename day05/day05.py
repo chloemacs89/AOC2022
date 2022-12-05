b72b509335b13b2e64cb274efd672d1e8b42ab2a
chloemacs89 <jer.villey@gmail.com>
2022-12-05 22:15:48 +0100

    day 05, 1st stars. Parsing was painful !

--- day05/day05.py
+++ day05/day05.py
@@ -1,17 +1,28 @@
 from collections import deque
 import re
 
-with open("./test.txt", "r", encoding="utf-8") as data_file:
+with open("./data.txt", "r", encoding="utf-8") as data_file:
     data = data_file.read().split("\n\n")
 
-crates = deque()
+crates = []
 
-for x in list(zip(*data[0].replace(" ", "-").split("\n"))):
-    current_crates = list(re.sub(r"\W+|\d", "", "".join(x)))
+for x in list(zip(*data[0].split("\n"))):
+    current_crates = deque((re.sub(r"\W+|\d", "", "".join(x))))
     if current_crates:
         crates.append(current_crates)
 
-program = [tuple(map(int, list(y))) for y in [re.sub(r"\D", "", x) for x in data[1].split("\n")]]
+program = [
+    tuple(map(int, y.lstrip().split(" ")))
+    for y in [re.sub(r"\D+", " ", x) for x in data[1].rstrip().split("\n")]
+]
 
-def move_crates(program, crates):
-    pass
+
+# first star
+def move_crates(program: list, crates: list[deque]):
+    for n in program:
+        for x in range(n[0]):
+            crates[n[2] - 1].appendleft(crates[n[1] - 1].popleft())
+
+
+move_crates(program, crates)
+print("".join([x[0] for x in crates if x]))
